9fc265dfa3494b70465f214e667ccf2da605ba1a
abir-la <a.laaroussi@etu.enset-media.ac.ma>
2023-03-27 12:04:59 +0000

    Initial commit

---
+++ src/main/java/ws/ServerJWS.java
@@ -0,0 +1,13 @@
+package ws;
+
+import ws.BanqueService;
+import jakarta.xml.ws.Endpoint;
+
+
+public class ServerJWS {
+    public static void main(String[] args) {
+        String url="http://0.0.0:9191/";
+        Endpoint.publish(url, new BanqueService());
+        System.out.println("Web service deploye sur"+url);
+    }
+}
